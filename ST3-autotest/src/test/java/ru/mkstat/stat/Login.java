3aa945d1c5bc7780281ef33f861778f3c787db82
Alexandertm13 <tm13.msk@gmail.com>
2017-07-04 17:21:39 +0300

    4

---
+++ ST3-autotest/src/test/java/ru/mkstat/stat/Login.java
@@ -0,0 +1,44 @@
+package ru.mkstat.stat;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+  private WebDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://192.168.50.45/";
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitled() throws Exception {
+    driver.get("http://192.168.50.45/");
+    driver.findElement(By.id("login-password")).clear();
+    driver.findElement(By.id("login-password")).sendKeys("master11");
+    driver.findElement(By.id("login-username")).clear();
+    driver.findElement(By.id("login-username")).sendKeys("admin");
+    driver.findElement(By.id("btn-login")).click();
+    driver.findElement(By.linkText("Данные статистики")).click();
+
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+
+    }
+  }
+
+
+
--- STATR/src/test/java/ru/stqa/pft/statr/appmanager/DictionaryKOPUK.java
+++
@@ -1,7 +0,0 @@
-package ru.stqa.pft.statr.appmanager;
-
-/**
- * Created by artur.petin on 03.10.2016.
- */
-public class DictionaryKOPUK {
-}
