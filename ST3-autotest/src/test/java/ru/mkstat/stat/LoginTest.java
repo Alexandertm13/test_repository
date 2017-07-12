e07e88a919bfe6fe00d4305bc68597b5ab7f2943
Alexandertm13 <tm13.msk@gmail.com>
2017-07-12 17:11:26 +0300

    ch2

--- ST3-autotest/src/test/java/ru/mkstat/stat/LoginTest.java
+++ ST3-autotest/src/test/java/ru/mkstat/stat/LoginTest.java
@@ -24,7 +24,7 @@ public class LoginTest { ChromeDriver wd;
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver",  "\\Users\\artur\\Desktop\\buildAgent\\chromedriver");
+    System.setProperty("webdriver.chrome.driver",  "\\Users\\artur\\Desktop\\buildAgent\\chromedriver.zip");
     wd = new ChromeDriver();
     baseUrl = "http://192.168.50.45/";
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
